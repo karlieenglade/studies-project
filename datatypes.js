347b8fd55d8eaf313d86cbb308c326a3671d69c2
Karlie Englade <karlieenglade@gmail.com>
2025-09-11 09:58:45 -0500

    variables and data types

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,58 @@
 /**
  * DATATYPES:
  * 
- * 0: (introduction)
+ * 0: Different types of values are stored into different types of data. Data is categorized by its type. There are
+ *    two main datatypes: simple/primitive and complex. 
+ * 1. Simple/primitive data types are: numbers, strings, Booleans, NaN (number), undefined, and null. These data types
+ *    are immutable, or unchanging, and do not collect other values.
+ * 
+ * 2. Complex data types are: arrays, objects, and functions. These data types collect other values.
+ *    
+ * 3: When passed into a function, simple and complex data types are copied differently. Simple data types are copied 
+ *    by value, but complex data types are copied by reference.
  * 
  */
+
+// simple data types
+    // immutable, unchangeable, do not collect values
+let num = 4; 
+let str = "hi";
+let bool = true;
+let nana = NaN;
+let hmm = undefined;
+let nothing = null;
+
+//operations on simple data types return new values and do not change original vlaue
+let num2 = num + 10;
+console.log(num); // 4 <-- unchanged
+console.log(num2); // 14 <-- new value
+
+//simple data types are copied by value when assigining or passing
+    //this means the value they represent remains unchanged 
+let a = 20; 
+//assigning variable b to the value of variable a
+let b = a; 
+console.log(b); // 20
+//now if we change the value of b 
+b = 50; 
+//b is now equal to 50, while a remains unchanged
+console.log(b); // 50
+console.log(a); // 20
+
+
+
+// complex data types
+    // collect other values
+let array = [1, "hello", true, [3, 5], {name: "Bob"}];
+let object = {name: "Mark", age: 93, mood: "excited"};
+
+//complex data types are copied by reference when assigning or passing
+    //this means the values they hold can be changed
+//changing first index of array to 5
+array[0] = 5;
+//printing the array shows that the original array was modified 
+console.log(array); // [5, "hello", true, [3, 5], {name: "Bob"}]
+//adding a property to object
+object.city = "New Orleans";
+//printing the object shows that the original object was modified
+console.log(object); // {name: "Mark", age: 93, mood: "excited", city: "New Orleans"}
--- variables.js
+++ variables.js
@@ -1,6 +1,136 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
+ * 0: Variables have unique names and store values of all data types. 
+ * 
+ * 1. Declaration and assignment: a variable declaration lets us know it exists, but it does not hold value until 
+ *    it is assigned. To declare a variable we use a keyword (var, let, or const) followed by the variable's unique
+ *    name using camelCase. To assign the variable to a value, we follow the variable name by "=" and the value. Variables can be 
+ *    reassigned.
+ *    
+ * 2. Var, let, const 
+ * 
+ * 3. Hoisting
  * 
  */
+
+//variable declaration
+var cat;
+//variable assignment
+cat = "Lenore";
+//declaration and assignment can happen all at once
+var dog = "Doodle";
+//a variable that is declared without being initialized is recognized but returns undefined
+var pig;
+console.log(pig); // undefined
+//variable names are made using camelCase for easy readability
+var thisIsCamelCase; 
+var thisisnotcamelcase;
+
+//var variables 
+    // global or function-scoped
+var name = "Ruby";
+if (name === "Max"){
+  console.log("Max is here");
+} else if (name === "Ruby"){
+  console.log("Ruby is here");
+} 
+// prints "Ruby is here" because var variable is global-scoped and can be accessed from within code blocks and functions 
+
+function addSound(animal){
+    var catSays = "meow";
+    return catSays + ", hiss";
+  }
+console.log(catSays); // ReferenceError: catSays is not defined <-- because var catSays is function-scoped
+
+    // can be redeclared and reassigned
+//declaring once
+var table; 
+//declaring twice and assigning
+var table = "brown";
+console.log(table); // "brown" <-- no error because re-declaring is ok
+//reassigning table to "green"
+table = "green";
+console.log(table); // "green" 
+
+
+    // hoisted but undefined
+//we can access a var variable before it is initialized or assigned 
+console.log(horse); // undefined
+var horse = 5; 
+
+
+//let variables
+    //block-scoped
+if (true){
+    let color = "blue";
+} else {
+    let color = "red";
+}
+console.log(color); // ReferenceError: color if not defined
+
+    //cannot be redeclared but can be reassigned
+    //declaring once
+let shape;
+    //declaring twice and assigning
+let shape = "square";
+console.log(shape); // "SyntaxError": Identifier "shape" has already been defined
+
+    //declaring and assigning
+let x = 5;
+console.log(x); // 5
+    //reassigning
+x = 20; 
+console.log(x); // 20 <-- no issue reassigning let variable 
+
+    //hoisted but not initialized 
+console.log(c); // ReferenceError: Cannot access "c" before initialization
+let c = 10;
+
+
+//const variables
+    //block-scoped
+if (true){
+    const horn = "honk";
+  } else {
+    const horn = "beep";
+  }
+console.log(horn); //ReferenceError: horn is not defined <-- cannot access const in block from outside the block
+
+    //cannot be reassigned, but if value is complex data type, its contents can be modified
+    //assigning once
+const dino = "rawr";
+    //reassigning
+dino = "grr"; 
+console.log(dino); //TypeError: Assignment to a constant variable
+
+//const variable of complex data type
+const myArray = [1, 2, 3];
+//modifying the array
+myArray.unshift(0);
+console.log(myArray); // [0, 1, 2, 3] <-- const complex data types can be modified
+
+    //hoisted but not initialized
+console.log(lion); // ReferenceError: cannot access "lion" before initialization
+const lion = "rawr";
+
+    //must be initialized upon declaration 
+const tiger; 
+console.log(tiger); //SyntaxError: Missing initializer in const declaration
+
+
+
+// hoisting (continued)
+    // function declarations and their bodies are hoisted
+    //this means functions can be called and used before they are reached in the code
+console.log(add5(1)); // 6 <-- no issue
+function add5(x){
+  return x + 5;
+}
+
+    //however, this does not apply when function is assigned to a variable 
+console.log(add10(30)); // TypeError: add10 is not a function
+var add10 = function(x){
+  return x + 10;
+}
+
